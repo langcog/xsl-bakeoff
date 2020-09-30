c738613c59fc7d571fdea2cd94e15238b148063c
kachergis <george.kachergis@gmail.com>
2020-09-30 14:25:37 -0700

    combined cond_fits, made master group-fit DF, updated tilles Model, worked on writeup

--- fitting_functions.R
+++ fitting_functions.R
@@ -146,19 +146,19 @@ run_stochastic_model <- function(conds, model_name, parameters, SSE_only=F, prin
 }
 
 fit_stochastic_model <- function(model_name, conds, lower, upper) {
-  fit = DEoptim(run_stochastic_model, lower=lower, upper=upper, DEoptim.control(reltol=.001, NP=100, itermax=20), 
+  fit = DEoptim(run_stochastic_model, lower=lower, upper=upper, DEoptim.control(reltol=.001, NP=100, itermax=30), 
                 model_name=model_name, conds=conds, SSE_only=T)
   return(fit)
 }
 
-stochastic_models = c("guess-and-test","pursuit_detailed","trueswell2012")
+stochastic_models = c("guess-and-test","pursuit_detailed","trueswell2012","kachergis_sampling")
 
 # for group fits (all conditions per model)
 get_model_dataframe <- function(fits, conds) {
   mdf = tibble()
   for(model_name in names(fits)) {
     if(is.element(model_name, stochastic_models)) {
-      pars = fits[[model_name]]$pars
+      pars = fits[[model_name]]$pars # change once we have DEoptim objs
       mdat = run_stochastic_model(conds, model_name, pars)
     } else {
       pars = fits[[model_name]]$optim$bestmem
@@ -222,6 +222,14 @@ do_group_fits <- function() {
   group_fits[["strength"]] = fit_model("strength", combined_data, c(.001,.1), c(5,1))
   group_fits[["uncertainty"]] = fit_model("uncertainty", combined_data, c(.001,.1,.5), c(5,15,1))
   group_fits[["rescorla-wagner"]] = fit_model("rescorla-wagner", conds, c(1e-5,1e-5,1e-5), c(1,1,1))
+  
+  # ToDo: merge these in from group_stochastic_fits.Rdata
+  # load("fits/group_stochastic_fits.Rdata")
+  group_fits[["trueswell2012"]] = fit_stochastic_model("trueswell2012", combined_data, c(.0001,.0001), c(1,1))
+  group_fits[["guess-and-test"]] = fit_stochastic_model("guess-and-test", combined_data, c(.0001,.0001), c(1,1))
+  group_fits[["pursuit_detailed"]] = fit_stochastic_model("pursuit_detailed", combined_data, c(1e-5, 1e-5, 1e-5), c(1,1,1))
+  group_fits[["kachergis_sampling"]] = fit_stochastic_model("kachergis_sampling", combined_data, c(.001,.1,.5), c(5,15,1))
+  
   gfd = get_model_dataframe(group_fits, combined_data)
   save(group_fits, gfd, file="fits/group_fits.Rdata")
 }
@@ -229,33 +237,25 @@ do_group_fits <- function() {
 
 ## TESTING
 #pv = run_stochastic_model(conds, "trueswell2012", c(.1, .5)) # SSE=1.18
-
 #gt = run_stochastic_model(conds, "guess-and-test", c(.1, .5)) # SSE=1.14
-# so far: Iteration: 10 bestvalit: 0.932846 bestmemit:    0.685980    0.994259
-
 #pt = run_stochastic_model(conds, "pursuit_detailed", c(.2, .3, .05)) # SSE=4.06
-
 
 #tl = fit_model("tilles", conds[[1]], c(1e-5,1e-5,1e-5), c(1,1,1)) # run_model
 
-##### NOT RUN YET
+
+
 load("fits/group_fits.Rdata")
 for(m in names(group_fits)) { 
   print(paste(m, round(group_fits[[m]]$optim$bestval, 2)))
 }
 
 # temporary (until we finish full fits)
-group_fits[["trueswell2012"]] = list(pars = c(0.113666, 0.266792)) # SSE=.879
-group_fits[["guess-and-test"]] = list(pars = c(0.691312, 0.991726)) # SSE=.884
-group_fits[["pursuit_detailed"]] = list(pars = c(0.088324, 0.409523, 0.000059)) # SSE = 0.996221
-
-group_fits[["trueswell2012"]] = fit_stochastic_model("trueswell2012", combined_data, c(.0001,.0001), c(1,1))
-group_fits[["guess-and-test"]] = fit_stochastic_model("guess-and-test", combined_data, c(.0001,.0001), c(1,1))
-group_fits[["pursuit_detailed"]] = fit_stochastic_model("pursuit_detailed", combined_data, c(1e-5, 1e-5, 1e-5), c(1,1,1))
-
-gfd = get_model_dataframe(group_fits, combined_data)
-
-save(group_fits, gfd, file="fits/group_fits.Rdata")
+#group_fits[["trueswell2012"]] = list(pars = c(0.113666, 0.266792)) # SSE=.879
+#group_fits[["guess-and-test"]] = list(pars = c(0.691312, 0.991726)) # SSE=.884
+#group_fits[["pursuit_detailed"]] = list(pars = c(0.088324, 0.409523, 0.000059)) # SSE = 0.996
+#group_fits[["kachergis_sampling"]] = list(pars = c(0.15553, 11.04866, 0.99260)) # SSE = 0.407
+#gfd = get_model_dataframe(group_fits, combined_data) # 726 items per model
+#save(group_fits, gfd, file="fits/group_fits.Rdata")
 
 
 # use for fitting given model to each condition
@@ -277,12 +277,8 @@ fit_stochastic_by_cond <- function(mname, conds, lower, upper) {
   return(mod_fits)
 }
 
-cond_fits[["trueswell2012"]] = fit_stochastic_by_cond("trueswell2012", combined_data, c(.0001,.0001), c(1,1))
-cond_fits[["guess-and-test"]] = fit_stochastic_by_cond("guess-and-test", combined_data, c(.0001,.0001), c(1,1))
-cond_fits[["pursuit_detailed"]] = fit_stochastic_by_cond("pursuit_detailed", combined_data, c(1e-5, 1e-5, 1e-5), c(1,1,1))
 
-
-do_cond_fits <- function() {
+completed_cond_fits <- function() {
   cond_fits = list()
   cond_fits[["kachergis"]] = fit_by_cond(c("kachergis"), combined_data, c(.001,.1,.5), c(5,15,1))
   cond_fits[["uncertainty"]] = fit_by_cond(c("uncertainty"), combined_data, c(.001,.1,.5), c(5,15,1))
@@ -290,7 +286,13 @@ do_cond_fits <- function() {
   cond_fits[["novelty"]] = fit_by_cond(c("novelty"), combined_data, c(.001,.1,.5), c(5,15,1))
   cond_fits[["fazly"]] = fit_by_cond("fazly", combined_data, c(1e-10,2), c(2,20000)) 
   cond_fits[["Bayesian_decay"]] = fit_by_cond("Bayesian_decay", combined_data, c(1e-5,1e-5,1e-5), c(10,10,10)) 
-  cond_fits[["rescorla-wagner"]] = fit_by_cond("rescorla-wagner", conds, c(1e-5,1e-5,1e-5), c(1,1,1))
+  cond_fits[["rescorla-wagner"]] = fit_by_cond("rescorla-wagner", combined_data, c(1e-5,1e-5,1e-5), c(1,1,1))
+  
+  # stochastic models so far fitted with 20-30 iterations per cond -- should increase this (to 100, and perhaps refit a few times?)
+  cond_fits[["trueswell2012"]] = fit_stochastic_by_cond("trueswell2012", combined_data, c(.0001,.0001), c(1,1))
+  cond_fits[["guess-and-test"]] = fit_stochastic_by_cond("guess-and-test", combined_data, c(.0001,.0001), c(1,1))
+  cond_fits[["pursuit_detailed"]] = fit_stochastic_by_cond("pursuit_detailed", combined_data, c(1e-5, 1e-5, 1e-5), c(1,1,1))
+  cond_fits[["kachergis_sampling"]] = fit_stochastic_by_cond("kachergis_sampling", combined_data, c(.001,.1,.5), c(5,15,1))
   
   cfd <- get_model_dataframe_cond_fits(cond_fits, combined_data)
   save(cond_fits, cfd, file="fits/cond_fits.Rdata")
--- models/tilles.R
+++ models/tilles.R
@@ -11,8 +11,9 @@ shannon.entropy <- function(p) { # some NA values getting in here...
 
 
 model <- function(params, ord=c()) {
-	X <- params[1] # reinforcement parameter for current stimuli
-	B <- params[2] # inference parameter, regulating ME and prior info integration
+	X <- params[1] # reinforcement parameter for stimuli in current context
+	B <- params[2] # inference parameter, regulating ME and prior info integration 
+	# (applies either to known words that do not appear in the current context or to new words in the current context)
 	alpha_0 <- params[3] # baseline efficiency corresponding to maximal uncertainty about referent of target word
 	# all parms in [0,1]
 	# B=1 enforces ME: new word is associated with equal prob to any available new objects
@@ -21,54 +22,68 @@ model <- function(params, ord=c()) {
 	voc_sz = max(unlist(ord$words), na.rm=TRUE) # vocabulary size
 	ref_sz = max(unlist(ord$objs), na.rm=TRUE) # number of objects
 	
-	novel = rep(TRUE,voc_sz) # set novel[i]=0 once i has appeared
+	novel_w = rep(TRUE, voc_sz) # set novel[i]=0 once i has appeared
+	novel_o = rep(TRUE, ref_sz)
 	m <- matrix(0, voc_sz, ref_sz) # association matrix - probabilities
 	compScore = rep(0, nrow(ord$words))
 	# training
 	tr_w = as.integer(ord$words[1,])
 	tr_o = as.integer(ord$objs[1,])
-	m[tr_w,tr_o] = 1/length(tr_o) # eq 1
-	novel[tr_w] = FALSE
+	m[tr_w,tr_o] = 1/length(tr_w) # eq 1: 1/C, where C=|context| (current words and objs)
+	novel_w[tr_w] = FALSE
+	novel_o[tr_o] = FALSE
+	
 	for(t in 2:nrow(ord$words)) { 
 	  tr_w = as.integer(ord$words[t,])
 	  tr_w = tr_w[!is.na(tr_w)]
 	  tr_o = as.integer(ord$objs[t,])
 	  tr_o = tr_o[!is.na(tr_o)]
 		
-		Nprev = length(which(novel==FALSE)) # N_{t-1} for alpha_{t-1}
+		Nprev_w = length(which(novel_w==FALSE)) # N_{t-1} for alpha_{t-1}
+		Nprev_o = length(which(novel_o==FALSE)) 
 		
-		cur_novel = tr_o[which(novel[tr_o])] # ~ = current novel stimuli
-		cur_old = tr_o[which(!novel[tr_o])] # current trial's old stimuli
-		other_old = setdiff(which(!novel), cur_old) # stimuli appearing before, but not currently
-		novel[tr_o] = FALSE
+		# update rule for context omega_t depends on membership in
+		# 1) words and referents appearing for first time on trial t, or
+		# 2) words and referents not appearing on t, but that have appeared before
 		
-		N = length(which(novel==FALSE)) # number of distinct stimuli observed so far: N_t
+		# objects
+		cur_novel_o = tr_o[which(novel_o[tr_o])] # ~ = current novel objects
+		cur_old_o = tr_o[which(!novel_o[tr_o])] # current trial's old objects
+		other_old_o = setdiff(which(!novel_o), cur_old_o) # stimuli appearing before, but not currently
+		novel_o[tr_o] = FALSE
+		# words
+		cur_novel_w = tr_w[which(novel_w[tr_w])] # ~ = current novel stimuli
+		cur_old_w = tr_w[which(!novel_w[tr_w])] # current trial's old words
+		other_old_w = setdiff(which(!novel_w), cur_old_w) # stimuli appearing before, but not currently
+		novel_w[tr_w] = FALSE
+		
+		N = length(which(novel_o==FALSE)) # number of distinct objects appearing up to and including N_t
 		
 		# preprocessing of novel stimuli
-		if(length(cur_novel)!=0) {
-			m[cur_novel,cur_novel] = B/length(cur_novel) + (1-B)/N
-			m[cur_novel,cur_old] = (1-B) / N
-			m[cur_novel,other_old] = (1-B) / N
+		if(length(cur_novel_w)!=0) { # what if cur_novel_o / cur_old_o / other_old_o == NULL?
+			m[cur_novel_w,cur_novel_o] = B/length(cur_novel) + (1-B)/N # Eqn 2
+			m[cur_novel_w,cur_old_o] = (1-B) / N # Eqn 3
+			m[cur_novel_w,other_old_o] = (1-B) / N # Eqn 4 (obj not appearing on current t, but earlier)
 		}
 		
 		# alpha_{t-1} -- must be previous trial!
-		alpha = rep(0,voc_sz) # length(tr)
-		for(w in 1:voc_sz) {
-			alpha[w] = alpha_0 + (1-alpha_0) * (1-shannon.entropy(m[w,])/log(Nprev)) # eq 7
-		}
+		alpha = rep(NA, voc_sz) # length(tr)
+		for(w in tr_w) { # 1:voc_sz
+			alpha[w] = alpha_0 + (1-alpha_0) * (1-shannon.entropy(m[w,])/log(N)) # eq 7 - Nprev?
+		} 
 		
 		# now for all stimuli on the current trial...
 		# net gain of confidence for association w_i and o_j:
 		
-		flux = rep(0,voc_sz)
+		flux = rep(NA, voc_sz)
 		r <- matrix(0, voc_sz, voc_sz)
 		
 		# slow iterative method; rowSums(m) = 1
 		for(w in tr_w) {
-			flux[w] = sum(m[w,other_old]) / sum(m[w,tr_o]) # eq 5.1
+			flux[w] = sum(m[w,other_old_o]) / sum(m[w,tr_o]) # eq 5.1
 			r[w,tr_o] = X*m[w,tr_o]*flux[w] # eq 5.2
 			m[w,tr_o] = m[w,tr_o] + alpha[w]*r[w,tr_o] + (1-alpha[w])*(1/N - m[w,tr_o]) # eq 8
-			m[w,other_old] = m[w,other_old] - alpha[w]*X*m[w,other_old] + (1-alpha[w])*(1/N - m[w,other_old]) # eq 9
+			m[w,other_old_o] = m[w,other_old_o] - alpha[w]*X*m[w,other_old_o] + (1-alpha[w])*(1/N - m[w,other_old_o]) # eq 9
 		}
     
 
@@ -79,11 +94,11 @@ model <- function(params, ord=c()) {
 		# now update the other_old words
     # if params[3] < .268 on "block2_369-3x3hiCD" m[18,18] becomes negative at trial 9
 		flux = rep(0,voc_sz)
-		r <- matrix(0, voc_sz, voc_sz)
-		flux[other_old] = rowSums(m[other_old,tr_o]) / rowSums(m[other_old,other_old]) # eq 11.1
-		r[other_old,other_old] = B*mt[other_old,other_old]*flux[other_old] # eq 11.2
-		m[other_old,other_old] = mt[other_old,other_old] + alpha[other_old]*r[other_old,other_old] + (1-alpha[other_old])*(1/N - mt[other_old,other_old]) # eq 12
-		m[other_old,tr_o] = mt[other_old,tr_o] - alpha[other_old]*B*mt[other_old,tr_o] + (1-alpha[other_old])*(1/N - mt[other_old,tr_o]) # eq 13
+		r <- matrix(0, voc_sz, ref_sz)
+		flux[other_old_w] = rowSums(m[other_old_w,tr_o]) / rowSums(m[other_old_w,other_old_o]) # eq 11.1
+		r[other_old_w,other_old_o] = B*mt[other_old_w,other_old_o]*flux[other_old_w] # eq 11.2
+		m[other_old_w,other_old_o] = mt[other_old_w,other_old_o] + alpha[other_old_w]*r[other_old_w,other_old_o] + (1-alpha[other_old_w])*(1/N - mt[other_old_w,other_old_o]) # eq 12
+		m[other_old_w,tr_o] = mt[other_old_w,tr_o] - alpha[other_old_w]*B*mt[other_old_w,tr_o] + (1-alpha[other_old_w])*(1/N - mt[other_old_w,tr_o]) # eq 13
 		
 		compScore[t] = sum(diag(m))
 	}
