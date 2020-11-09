3dfdcdc266845b7e7ea7d29e8a69afe70556f3d3
George Kachergis <george.kachergis@gmail.com>
2020-11-08 16:09:09 -0800

    final group CV fit done!

--- cv_group_fits.R
+++ cv_group_fits.R
@@ -19,16 +19,13 @@ cv_group_fits <- function() {
   cv_group_fits[["pursuit_detailed"]] = cross_validated_group_fits("pursuit_detailed", combined_data, c(1e-5, 1e-5, 1e-5), c(1,1,1))
   # Error in m[novel[w], min_ref] <- gamma : 
   # number of items to replace is not a multiple of replacement length
-  #cv_group_fits[["kachergis_sampling"]] = cross_validated_group_fits("kachergis_sampling", combined_data, c(.001,.1,.5), c(5,15,1))
+  cv_group_fits[["kachergis_sampling"]] = cross_validated_group_fits("kachergis_sampling", combined_data, c(.001,.1,.5), c(5,15,1))
   
   save(cv_group_fits, file="fits/cv_group_fits.Rdata")
 }
 
 #cv_group_fits()
 load("fits/cv_group_fits.Rdata")
-#cv_group_fits[["guess-and-test"]] = cross_validated_group_fits("guess-and-test", combined_data, c(.0001,.0001), c(1,1))
-cv_group_fits[["kachergis_sampling"]] = cross_validated_group_fits("kachergis_sampling", combined_data, c(.001,.1,.5), c(5,15,1))
-# Error in temp_wts * nent : non-conformable arrays
 
 save(cv_group_fits, file="fits/cv_group_fits.Rdata")
 #for(m in names(cv_group_fits) print(paste(cv_group_fits[[m]]$
