36954ea0e349be8df6edb7f786f7128bd74aac1f
George Kachergis <george.kachergis@gmail.com>
2021-10-29 11:15:27 -0700

    fits for FGT and FM for all deterministic models except kachergis and uncertainty on FM

--- fit_CHILDES.R
+++ fit_CHILDES.R
@@ -5,8 +5,8 @@ source("optimize_corpus_fscores.R")
 #load in group_fits before running/sourcing
 load(here("fits","group_fits.Rdata"))
 
-determ_models = c("Bayesian_decay", "fazly", "kachergis", "novelty",
-                  "rescorla-wagner", "strength", "uncertainty")
+determ_models = c("Bayesian_decay", "fazly", "kachergis", 
+                  "novelty", "rescorla-wagner", "strength", "uncertainty")
 stochastic_models = c("guess-and-test","pursuit","trueswell2012","kachergis_sampling")
 
 #corpus_fits = list(FGT = list(), FM = list())
@@ -18,10 +18,15 @@ for (model_name in determ_models) {
   save(fit, file=here("fits","FGT_FM_fits.Rdata"))
 }
 
+determ_models = c("kachergis", "uncertainty") # re-fit for FM
+
 for (model_name in determ_models) {
   fit = optimize_corpus_fscore(fm_ord, model_name)
   corpus_fits[["FM"]][model_name] = fit
   save(fit, file=here("fits","FGT_FM_fits.Rdata"))
 }
+# problem for FM with kachergis, and with uncertainty: 
+#Error in if (min(p) < 0 || sum(p) <= 0) return(NA) : 
+#missing value where TRUE/FALSE needed
 
 # ToDo: fit stochastic models
