dc1c183abfc7708265487c0602fe45f90df72c14
George Kachergis <george.kachergis@gmail.com>
2021-11-05 13:42:21 -0700

    corrected f-score plot

--- fit_CHILDES.R
+++ fit_CHILDES.R
@@ -27,12 +27,8 @@ for (model_name in determ_models) {
   corpus_fits[["FM"]][model_name] = fit
   save(corpus_fits, file=here("fits","FGT_FM_fits.Rdata"))
 }
-# problem for FM with kachergis, and with uncertainty: 
-#Error in if (min(p) < 0 || sum(p) <= 0) return(NA) : 
-#missing value where TRUE/FALSE needed
 
-# ToDo: fit stochastic models
-
+# fit stochastic models
 for (model_name in stochastic_models) {
   fit = optimize_corpus_fscore(fgt_ord, model_name)
   corpus_fits[["FGT"]][model_name] = fit
@@ -45,17 +41,20 @@ for (model_name in stochastic_models) {
   save(fit, file=here("fits","FGT_FM_stoch_fits.Rdata"))
 }
 
-
-df <- tibble()
-for(corpus_name in names(corpus_fits)) {
-  for(model_name in names(corpus_fits[[corpus_name]])) {
-    df <- bind_rows(df, 
-                    tibble(corpus = corpus_name, 
-                           model = model_name, 
-                           fscore = corpus_fits[[corpus_name]][[model_name]]$bestval))
+get_corpus_fits_df <- function(corpus_fits) {
+  df <- tibble()
+  for(corpus_name in names(corpus_fits)) {
+    for(model_name in names(corpus_fits[[corpus_name]])) {
+      df <- bind_rows(df, 
+                      tibble(corpus = corpus_name, 
+                             model = model_name, 
+                             fscore = 1-corpus_fits[[corpus_name]][[model_name]]$bestval))
+    }
   }
+  return(df)
 }
 
+df <- get_corpus_fits_df(corpus_fits)
 df %>% ggplot(aes(y=model, x=fscore)) +
   facet_wrap(. ~ corpus) + geom_point() +
   theme_bw() + xlim(0,1)
